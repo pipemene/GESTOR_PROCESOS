23fad480110bad6149efe0961e3d32669c769439
pipemene <pipemene@icloud.com>
2025-10-11 15:13:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-export const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
-export const USERS_SHEET = process.env.USERS_SHEET;
-export const ORDERS_SHEET = process.env.ORDERS_SHEET;
-export const GOOGLE_SERVICE_ACCOUNT_EMAIL = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
-export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
-export const JWT_SECRET = process.env.JWT_SECRET;
+export const {
+  GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  GOOGLE_PRIVATE_KEY,
+  SPREADSHEET_ID,
+  USERS_SHEET,
+  ORDERS_SHEET,
+  JWT_SECRET,
+  DRIVE_FOLDER_ID // 👈 ESTA ES LA VARIABLE QUE FALTABA
+} = process.env;
