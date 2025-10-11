9a0a1e4006c908e862c4abf639aaa1c650b4a878
pipemene <pipemene@icloud.com>
2025-10-11 15:56:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,6 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const {
-  GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  GOOGLE_PRIVATE_KEY,
-  SPREADSHEET_ID,
-  USERS_SHEET,
-  ORDERS_SHEET,
-  JWT_SECRET,
-  DRIVE_FOLDER_ID,
-  GMAIL_USER,
-  GMAIL_APP_PASSWORD
-} = process.env;
+export const GOOGLE_SERVICE_ACCOUNT_EMAIL = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
+export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
+export const GOOGLE_SHEET_ID = process.env.GOOGLE_SHEET_ID;
+export const DRIVE_FOLDER_ID = process.env.DRIVE_FOLDER_ID;
+export const GMAIL_APP_USER = process.env.GMAIL_APP_USER;
+export const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD;
